7bf1b7a76236ec2165f662f2a720c15fe5e23eef
19WH1A0546 <81098296+19WH1A0546@users.noreply.github.com>
2021-09-07 21:09:19 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,2 @@
+r = float(input("Input the radius of the circle : "))
+print("The are of the circle with radius",r, "is", 22/7*r*r)
---
+++ file_extension.py
@@ -0,0 +1,7 @@
+filename = str(input("Input the filename: "))
+extension = filename[filename.index('.')+1:]
+if (extension == "py"):
+    print("The extension of the file is: 'python'")
+elif (extension == 'c'):
+    print("The extension of the file is: 'c'")
+
